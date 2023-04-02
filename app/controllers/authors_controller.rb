6f7e09e74022fcd3a6984a36704393019ac2fe09
esther <esther.githinji@student.moringaschool.com>
2023-04-02 11:26:56 +0300

    modified controller files and added cors.rb file

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -4,58 +4,19 @@ class AuthorsController < ApplicationController
   # GET /authors or /authors.json
   def index
     @authors = Author.all
+    render json: @authors
   end
 
   # GET /authors/1 or /authors/1.json
   def show
-  end
-
-  # GET /authors/new
-  def new
-    @author = Author.new
-  end
-
-  # GET /authors/1/edit
-  def edit
-  end
-
-  # POST /authors or /authors.json
-  def create
-    @author = Author.new(author_params)
-
-    respond_to do |format|
-      if @author.save
-        format.html { redirect_to author_url(@author), notice: "Author was successfully created." }
-        format.json { render :show, status: :created, location: @author }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @author.errors, status: :unprocessable_entity }
-      end
+    # @author = Author.find_by(id: params[:id])
+    if @author
+      render json: @author
+    else 
+      render json: {error:"Author not found"}, status: :not_found
     end
   end
-
-  # PATCH/PUT /authors/1 or /authors/1.json
-  def update
-    respond_to do |format|
-      if @author.update(author_params)
-        format.html { redirect_to author_url(@author), notice: "Author was successfully updated." }
-        format.json { render :show, status: :ok, location: @author }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @author.errors, status: :unprocessable_entity }
-      end
-    end
-  end
-
-  # DELETE /authors/1 or /authors/1.json
-  def destroy
-    @author.destroy
-
-    respond_to do |format|
-      format.html { redirect_to authors_url, notice: "Author was successfully destroyed." }
-      format.json { head :no_content }
-    end
-  end
+ 
 
   private
     # Use callbacks to share common setup or constraints between actions.

78809cd446d3b6bcd375f706f20c2a72354a2eb7
esther <esther.githinji@student.moringaschool.com>
2023-04-02 14:10:29 +0300

    seeding data

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  Rails.application.routes.draw do
   resources :reviews
   resources :books 
   
@@ -11,5 +10,4 @@ Rails.application.routes.draw do
   get "/me", to: "users#show"
   delete "/logout", to: "sessions#destroy"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-end
 end

f639344ce83627452bb5e67e669e8ee478a887a8
patrick kisyula <pmusyimi42@gmail.com>
2023-04-03 16:46:07 +0300

    debbugging

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
-Rails.application.config.middleware.insert_before 0, Rack::Cors do
-  allow do
-    origins 'library-front-end-xnuw.vercel.app'
-    resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true, expose: ['Access-Control-Allow-Origin']
-  end
-end
+# Rails.application.config.middleware.insert_before 0, Rack::Cors do
+#   allow do
+#     origins 'library-front-end-xnuw.vercel.app'
+#     resource '*', headers: :any, methods: [:get, :post, :put, :patch, :delete, :options, :head], credentials: true, expose: ['Access-Control-Allow-Origin']
+#   end
+# end
